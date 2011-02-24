2154bfa847d1e9ac1ce5552f8871f0921f158496
egoholic <egotraumatic@gmail.com>
2011-02-24 16:49:22 +0200

    delete unnecessary files

--- lib/free_relic/f.rb
+++
@@ -1,4 +0,0 @@
-require 'yaml'
-a = {'development'=>{"auth"=>{"active"=>true, "login"=> 'root', "password"=>'root'}}, 'test'=>{"auth"=>{"active"=>true, "login"=> 'root', "password"=>'root'}}, 'production'=>{"auth"=>{"active"=>true, "login"=> 'root', "password"=>'root'}}}.to_yaml
-
-puts a
