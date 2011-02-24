f2f5b5461d5a0c1c031e0d1486e792298ad83f03
egoholic <egotraumatic@gmail.com>
2011-02-24 15:44:56 +0200

    edited authentication

--- app/controllers/free_relic/base_controller.rb
+++ app/controllers/free_relic/base_controller.rb
@@ -11,6 +11,17 @@ class FreeRelic::BaseController < ApplicationController
     filters.each{ |filter| FreeRelic::BaseController.send(:skip_filter, filter) }
   end
 
+  def authenticate
+    authenticate_or_request_with_http_basic do |l, p|
+      if FREERELIC_CONFIG[:auth][:active]
+        (l == FREERELIC_CONFIG[:auth][:login] && p == FREERELIC_CONFIG[:auth][:password]) ? true : false
+
+      else
+        true
+      end
+    end
+  end
+
   protected
 
   def mute_notifications
--- app/controllers/free_relic/metrics_controller.rb
+++ app/controllers/free_relic/metrics_controller.rb
@@ -1,4 +1,6 @@
 class FreeRelic::MetricsController < FreeRelic::BaseController
+  before_filter :authenticate
+
   def index
     # Need to swap page and scope when bug https://github.com/amatsuda/kaminari/issues#issue/26 will be fixed
     @metrics = FreeRelic::Metric.page(params[:page]).sorted
