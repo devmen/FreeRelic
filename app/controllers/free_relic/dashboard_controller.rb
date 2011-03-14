ca40e84dd482957e43ffb2dfc279548245183688
egoholic <egotraumatic@gmail.com>
2011-03-14 14:24:07 +0200

    add controllers for specific notifications

--- app/controllers/free_relic/dashboard_controller.rb
+++ app/controllers/free_relic/dashboard_controller.rb
@@ -4,6 +4,7 @@ class FreeRelic::DashboardController < FreeRelic::BaseController
     @latest_metrics       = FreeRelic::Metric.sorted.limit(5)
     @slowest_sql_metrics  = FreeRelic::Metric.sql_metrics.by_duration.limit(5)
     @slowest_http_metrics = FreeRelic::Metric.http_metrics.by_duration.limit(5)
+    #there must be some error notifications
     @errors               = {}
   end
 
